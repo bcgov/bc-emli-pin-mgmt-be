b740efaf2ce8d5b6c3e49ce3ad6d932b2347cd0f
hannah-macdonald1 <hamacdonald@deloitte.ca>
2023-07-25 09:44:52 -0600

    Minor typeorm fix (ts rules were too strict and showing errors in exported calsses with required fields)

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -3,13 +3,13 @@ import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 @Entity()
 export class User {
     @PrimaryGeneratedColumn()
-    id!: number;
+    id: number;
 
     @Column()
-    firstName!: string;
+    firstName: string;
 
     @Column()
-    lastName!: string;
+    lastName: string;
 
     @Column()
     age!: number;
