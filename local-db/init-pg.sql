acaea74608e5bdc1fc372d0acb91b927b2ca1127
ceciliahangzhou <cezhou@deloitte.ca>
2023-07-28 13:17:28 -0400

    user table modification

--- local-db/init-pg.sql
+++ local-db/init-pg.sql
@@ -15,7 +15,7 @@ CREATE TABLE IF NOT EXISTS "user" (
     user_name VARCHAR(50) NOT NULL,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(75) NOT NULL,
-    is_active BOOLEAN NOT NULL,
+    is_active BOOLEAN NOT NULL
 );
 
 -- ACTIVE PIN -------------------------------------------
