c65f65339b713189ffd1a544a597c26fff25d704
hannah-macdonald1 <hamacdonald@deloitte.ca>
2023-08-02 10:53:44 -0600

    fixed typeorm migrations

--- src/migration/1690919775722-migration.ts
+++ src/migration/1690919775722-migration.ts
@@ -16,11 +16,11 @@ export class Create1690919775722 implements MigrationInterface {
             `CREATE TYPE ${schemaName}.role_type AS ENUM ('Standard', 'Admin', 'SuperAdmin');`,
         );
 
-        await queryRunner.query(`CREATE TABLE IF NOT EXISTS "users" (
+        await queryRunner.query(`CREATE TABLE IF NOT EXISTS ${schemaName}."users" (
 			user_id UUID PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),
 			user_guid VARCHAR(36) NOT NULL,
 			identity_type VARCHAR(10) NOT NULL,
-			role role_type NOT NULL, 
+			role ${schemaName}.role_type NOT NULL, 
 			organization VARCHAR(50) NOT NULL,
 			email CITEXT NOT NULL,
 			user_name VARCHAR(50) NOT NULL,
@@ -55,14 +55,14 @@ export class Create1690919775722 implements MigrationInterface {
             `CREATE TYPE ${schemaName}.title_status_type AS ENUM ('R', 'C');`,
         );
 
-        await queryRunner.query(`CREATE TABLE IF NOT EXISTS "active_pin" (
+        await queryRunner.query(`CREATE TABLE IF NOT EXISTS ${schemaName}."active_pin" (
 			live_pin_id UUID PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),
 			pin VARCHAR(8),
 			pid INT NOT NULL,
-			parcel_status parcel_status_type NOT NULL,
+			parcel_status ${schemaName}.parcel_status_type NOT NULL,
 			title_number VARCHAR(11) NOT NULL,
 			land_title_district VARCHAR(2) NOT NULL,
-			title_status title_status_type NOT NULL,
+			title_status ${schemaName}.title_status_type NOT NULL,
 			from_title_number VARCHAR(11),
 			from_land_title_district VARCHAR(2),
 			given_name VARCHAR(50),
@@ -84,10 +84,10 @@ export class Create1690919775722 implements MigrationInterface {
 		-- PERMISSION -------------------------------------------
 		-- Table Definition ----------------------------------------------
 		*/
-        await queryRunner.query(`CREATE TABLE IF NOT EXISTS "permission" (
+        await queryRunner.query(`CREATE TABLE IF NOT EXISTS ${schemaName}."permission" (
 			permission_id UUID PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),
 			permission VARCHAR(50) NOT NULL,
-			role role_type NOT NULL
+			role ${schemaName}.role_type NOT NULL
 		);`);
 
         /*
@@ -107,18 +107,18 @@ export class Create1690919775722 implements MigrationInterface {
             `CREATE TYPE ${schemaName}.expiration_reason_type AS ENUM ('OP', 'CC', 'OR', 'CO');`,
         );
 
-        await queryRunner.query(`CREATE TABLE IF NOT EXISTS "pin_audit_log" (
+        await queryRunner.query(`CREATE TABLE IF NOT EXISTS ${schemaName}."pin_audit_log" (
 			log_id UUID PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),
 			pin VARCHAR(8) NOT NULL,
 			pid INT NOT NULL,
-			parcel_status parcel_status_type NOT NULL,
+			parcel_status ${schemaName}.parcel_status_type NOT NULL,
 			title_number VARCHAR(11) NOT NULL, 
 			land_title_district VARCHAR(2) NOT NULL,
 			from_title_number VARCHAR(11),
 			from_land_title_district VARCHAR(2),
-			title_status title_status_type NOT NULL,
+			title_status ${schemaName}.title_status_type NOT NULL,
 			expired_at TIMESTAMP, 
-			expiration_reason expiration_reason_type, 
+			expiration_reason ${schemaName}.expiration_reason_type, 
 			sent_to_email CITEXT, 
 			sent_to_phone VARCHAR(12)
 		);`);
@@ -127,11 +127,16 @@ export class Create1690919775722 implements MigrationInterface {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     public async down(queryRunner: QueryRunner): Promise<void> {
         const schemaName = process.env.NODE_ENV === 'test' ? 'test' : 'db';
-        await queryRunner.dropTable('users', true);
-        await queryRunner.dropTable('active_pin', true);
-        await queryRunner.dropTable('permission', true);
-        await queryRunner.dropTable('pin_audit_log', true);
-        await queryRunner.query(`DROP TYPE IF EXISTS ${schemaName}.role_type`);
+        await queryRunner.query(`DROP TABLE IF EXISTS ${schemaName}."users"`);
+        await queryRunner.query(
+            `DROP TABLE IF EXISTS ${schemaName}."pin_audit_log"`,
+        );
+        await queryRunner.query(
+            `DROP TABLE IF EXISTS ${schemaName}."permission"`,
+        );
+        await queryRunner.query(
+            `DROP TABLE IF EXISTS ${schemaName}."active_pin"`,
+        );
         await queryRunner.query(
             `DROP TYPE IF EXISTS ${schemaName}.parcel_status_type`,
         );
