b910da00c4de4100693dcb064569ea9fbfef6621
hannah-macdonald1 <hamacdonald@deloitte.ca>
2023-08-11 13:05:55 -0600

    added db query

--- src/db/ActivePIN.db.ts
+++ src/db/ActivePIN.db.ts
@@ -1,5 +1,9 @@
+import { UpdateResult } from 'typeorm';
 import { AppDataSource } from '../data-source';
 import { ActivePin } from '../entity/ActivePin';
+import { PinAuditLog } from '../entity/PinAuditLog';
+import { expirationReason } from '../helpers/types';
+import logger from '../middleware/logger';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export async function findPin(
@@ -18,4 +22,33 @@ export async function findPin(
         result = await PINRepo.find(query);
     }
     return result;
+}
+
+export async function deletePin(
+    id: string,
+    reason: expirationReason,
+    expiredByName: string,
+    expiredByUsername: string,
+) {
+    let PINToDelete: ActivePin | undefined;
+    let logInfo: UpdateResult | undefined;
+    await AppDataSource.transaction(async (manager) => {
+        PINToDelete = await manager.findOneOrFail(ActivePin, {
+            where: { livePinId: id },
+        });
+        await manager.remove(ActivePin, PINToDelete); // deletes and creates an entry in the audit log via trigger
+        // Update deletion info with expiration reason
+        logInfo = await manager.update(
+            PinAuditLog,
+            { livePinId: id },
+            { expirationReason: reason, expiredByName, expiredByUsername },
+        );
+    });
+    if (typeof logInfo != 'undefined') {
+        if (logInfo.affected && logInfo.affected === 1) {
+            logger.debug(
+                `Successfully deleted ActivePIN with live_pin_id '${id}'`,
+            );
+        }
+    }
 }
