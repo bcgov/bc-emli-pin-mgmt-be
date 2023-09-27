f429bde2d7d3f94ab461e926ae6fa7be8c1fd2a3
emilymendelson <emendelson@deloitte.ca>
2023-09-27 09:25:48 -0400

    Adding active_pin to migration

--- src/migration/1695777726084-migration.ts
+++ src/migration/1695777726084-migration.ts
@@ -48,6 +48,19 @@ export class Migration1695777726084 implements MigrationInterface {
         await queryRunner.query(
             `ALTER TABLE IF EXISTS ${schemaName}.titleowner_raw ADD CONSTRAINT unique_constraint_titleowner UNIQUE (unique_key)`,
         );
+
+        await queryRunner.query(
+            `ALTER TABLE IF EXISTS ${schemaName}.active_pin ADD COLUMN IF NOT EXISTS unique_key VARCHAR(500)`,
+        );
+        await queryRunner.query(
+            `ALTER TABLE IF EXISTS ${schemaName}.active_pin DROP CONSTRAINT IF EXISTS unique_constraint_active_pin`,
+        );
+        await queryRunner.query(
+            `ALTER TABLE IF EXISTS ${schemaName}.active_pin ADD CONSTRAINT unique_constraint_active_pin UNIQUE (unique_key)`,
+        );
+        await queryRunner.query(
+            `ALTER TABLE IF EXISTS ${schemaName}.active_pin ALTER COLUMN title_status TYPE VARCHAR(1)`,
+        );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
@@ -96,6 +109,22 @@ export class Migration1695777726084 implements MigrationInterface {
         );
         await queryRunner.query(
             `ALTER TABLE IF EXISTS ${schemaName}.titleowner_raw DROP CONSTRAINT IF EXISTS unique_constraint_titleowner`,
+        );
+
+        await queryRunner.query(
+            `ALTER TABLE IF EXISTS ${schemaName}.active_pin DROP COLUMN IF EXISTS unique_key`,
+        );
+        await queryRunner.query(
+            `ALTER TABLE IF EXISTS ${schemaName}.active_pin DROP CONSTRAINT IF EXISTS unique_constraint_active_pin`,
+        );
+        await queryRunner.query(
+            `DROP TYPE IF EXISTS ${schemaName}.title_status_type`,
+        );
+        await queryRunner.query(
+            `CREATE TYPE ${schemaName}.title_status_type AS ENUM ('R', 'C');`,
+        );
+        await queryRunner.query(
+            `ALTER TABLE IF EXISTS ${schemaName}.active_pin ALTER COLUMN title_status TYPE title_status_type`,
         );
     }
 }
