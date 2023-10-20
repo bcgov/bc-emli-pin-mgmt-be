3998719a66bdf575bb0ac3e60623c757f95a9eaa
hannah-macdonald1 <hamacdonald@deloitte.ca>
2023-10-20 15:18:57 -0600

    Fixed data source tests to not require the experimental dynmaic import

--- src/tests/data-source.spec.ts
+++ src/tests/data-source.spec.ts
@@ -8,33 +8,20 @@ const prevValues = {
     TYPEORM_LOGGING: process.env.TYPEORM_LOGGING,
 };
 
+if (process.env.NODE_ENV === 'test') delete process.env.NODE_ENV;
+if (process.env.DB_HOST !== '127.0.0.1') process.env.DB_HOST = '127.0.0.1';
+else delete process.env.DB_HOST;
+if (process.env.DB_PORT !== '5432') delete process.env.DB_PORT;
+else process.env.DB_PORT = '1111';
+process.env.TYPEORM_SYNCHRONIZE = 'true';
+process.env.TYPEORM_LOGGING?.toLowerCase() === 'true'
+    ? (process.env.TYPEORM_LOGGING = 'false')
+    : (process.env.TYPEORM_LOGGING = 'true');
+
+import { AppDataSource } from '../data-source';
 describe('data-source tests', () => {
-    beforeAll(async () => {
-        // Do the standard import first. We need the ts ignore because ts is being dumb here
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        const { AppDataSource } = await import('./../data-source');
-        jest.resetModules();
-    });
-    test('data-source should initialize with standard configuration', async () => {
-        // This is just for semantics, it actually runs in the beforeAll to guarantee order.
-    });
-    test('data-source should initialize with alternate configuration', async () => {
-        if (process.env.NODE_ENV === 'test') delete process.env.NODE_ENV;
-        if (process.env.DB_HOST !== '127.0.0.1')
-            process.env.DB_HOST = '127.0.0.1';
-        else delete process.env.DB_HOST;
-        if (process.env.DB_PORT !== '5432') delete process.env.DB_PORT;
-        else process.env.DB_PORT = '1111';
-        process.env.TYPEORM_SYNCHRONIZE = 'true';
-        process.env.TYPEORM_LOGGING?.toLowerCase() === 'true'
-            ? (process.env.TYPEORM_LOGGING = 'false')
-            : (process.env.TYPEORM_LOGGING = 'true');
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        const { AppDataSource } = await import('./../data-source');
+    test('import AppDataSource with different parameters', async () => {
+        expect(AppDataSource.options.type).toBe('postgres');
     });
     afterAll(async () => {
         process.env.NODE_ENV = prevValues.NODE_ENV;
