0db6e996d3816644841fa4cb86049137b378495f
hannah-macdonald1 <hamacdonald@deloitte.ca>
2023-10-21 15:44:14 -0600

    pidHelpers test

--- src/tests/helpers/pidHelpers.spec.ts
+++ src/tests/helpers/pidHelpers.spec.ts
@@ -1,4 +1,9 @@
-import { pidStringSort, pidStringSplitAndSort } from '../../helpers/pidHelpers';
+import { ActivePin } from '../../entity/ActivePin';
+import {
+    pidStringSort,
+    pidStringSplitAndSort,
+    sortActivePinResults,
+} from '../../helpers/pidHelpers';
 
 describe('pidHelpers tests', () => {
     test('pidStringSplitAndSort returns an array of size 1 if there is a single valid input of type string', () => {
@@ -46,5 +51,36 @@ describe('pidHelpers tests', () => {
         expect(() => {
             pidStringSort('');
         }).toThrow(`Error in pidStringSort: No pid to parse in input ''`);
+    });
+
+    test('sortActivePinResults works with multiple entries for the same title', () => {
+        const pin1 = new ActivePin();
+        pin1.pids = '1234|5678';
+        pin1.titleNumber = 'EFGH';
+        pin1.landTitleDistrict = 'BC';
+        pin1.livePinId = 'cf430240-e5b6-4224-bd71-a02e098cc6e8';
+        pin1.lastName_1 = 'None';
+        pin1.incorporationNumber = '91011';
+        pin1.addressLine_1 = '123 example st';
+        pin1.city = 'Vancouver';
+        pin1.provinceAbbreviation = 'BC';
+        pin1.country = 'Canada';
+        pin1.postalCode = 'V1V1V1';
+        const pin2 = new ActivePin();
+        pin2.pids = '1234|5678';
+        pin2.titleNumber = 'EFGH';
+        pin2.landTitleDistrict = 'BC';
+        pin2.livePinId = 'cf430240-e5b6-4224-bd71-a02e098cc6e8';
+        pin2.lastName_1 = 'ABCD';
+        pin2.incorporationNumber = '91011';
+        pin2.addressLine_1 = '123 example st';
+        pin2.city = 'Vancouver';
+        pin2.provinceAbbreviation = 'BC';
+        pin2.country = 'Canada';
+        pin2.postalCode = 'V1V1V1';
+        const result = [pin1, pin2];
+        const sortedResults = sortActivePinResults(result);
+        expect(sortedResults['EFGH|BC']).toBeDefined();
+        expect(sortedResults['EFGH|BC'].length).toBe(2);
     });
 });
