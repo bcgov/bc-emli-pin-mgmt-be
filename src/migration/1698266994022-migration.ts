9ab7980834afe0c8e1feacd7f1211fcfcb0a2ec3
hannah-macdonald1 <hamacdonald@deloitte.ca>
2023-10-26 11:06:11 -0600

    update migration to delete anything currently in permission table before adding new entries

--- src/migration/1698266994022-migration.ts
+++ src/migration/1698266994022-migration.ts
@@ -3,6 +3,7 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class Migration1698266994022 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         const schemaName = process.env.NODE_ENV === 'test' ? 'test' : 'public';
+        await queryRunner.query(`DELETE FROM ${schemaName}."permission";`);
         await queryRunner.query(`INSERT INTO ${schemaName}."permission" ("permission","role") VALUES
 		('USER_ACCESS','SuperAdmin'),
 		('VIEW_PIN','SuperAdmin'),
