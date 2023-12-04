ec43d1a860b22c6994b4f4489cb61c5fc00ca996
hannah-macdonald1 <hamacdonald@deloitte.ca>
2023-12-04 09:49:05 -0700

    Vhers audit log table creation

---
+++ src/entity/VhersAuditLog.ts
@@ -0,0 +1,45 @@
+import { Column, Entity, Index } from 'typeorm';
+
+@Index('vhers_audit_log_pkey', ['logId'], { unique: true })
+@Entity('vhers_audit_log')
+export class VhersAuditLog {
+    @Column('uuid', {
+        primary: true,
+        name: 'log_id',
+        default: () => 'uuid_generate_v4()',
+    })
+    logId: string;
+
+    @Column('real', { name: 'response_time_ms', precision: 24 })
+    responseTimeMs: number;
+
+    @Column('enum', {
+        name: 'endpoint_name',
+        enum: [
+            '/pins/vhers-create',
+            '/pins/vhers-regenerate',
+            '/pins/verify',
+            '/virus-scan',
+        ],
+    })
+    endpointName:
+        | '/pins/vhers-create'
+        | '/pins/vhers-regenerate'
+        | '/pins/verify'
+        | '/virus-scan';
+
+    @Column('integer', { name: 'status_code' })
+    statusCode: number;
+
+    @Column('timestamp with time zone', {
+        name: 'created_at',
+        default: () => 'now()',
+    })
+    createdAt: Date;
+
+    @Column('json', { name: 'request_body', nullable: true })
+    requestBody: object | null;
+
+    @Column('json', { name: 'response_body', nullable: true })
+    responseBody: object | null;
+}
