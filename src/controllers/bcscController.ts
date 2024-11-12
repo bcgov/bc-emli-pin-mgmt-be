e9650a6ff60182d2c7b1375d6247286d83f4507b
Darrel Siegle <darrel.siegle@dxcas.com>
2024-11-12 09:30:05 -0800

    feat: bcsc sign-in

--- src/controllers/bcscController.ts
+++ src/controllers/bcscController.ts
@@ -2,26 +2,26 @@
 import { Controller, Get, Query, Res, Route, Tags, TsoaResponse } from 'tsoa';
 
 import { getAuthorizationUrl } from '../helpers/auth';
+import { pidStringSplitAndSort, getPIDs } from '../helpers/pidHelpers';
+import {
+    getAddressResults,
+    badRequestError,
+    serverErrorType,
+    pidNotFound,
+    GenericTypeORMErrorType,
+} from '../helpers/types';
+import GeocodeAPICaller from '../helpers/geocodeAPICaller';
+import { compareNames } from '../helpers/nameMatching';
+
 import logger from '../middleware/logger';
+
+import { findPropertyDetails, updateActivePin } from '../db/ActivePIN.db';
+import { TypeORMError } from 'typeorm';
 
 interface UserInfoResponse {
     success: boolean;
-    token?: string;
-    refreshToken?: string;
-    userInfo?: {
-        sub: string;
-        identity_provider: string;
-        address: {
-            street_address: string;
-            locality: string;
-            region: string;
-            postal_code: string;
-        };
-        email_verified: boolean;
-        preferred_username: string;
-        given_names: string;
-        family_name: string;
-    };
+    pids: string;
+    livePinId: string;
 }
 
 @Route('bcsc')
@@ -31,6 +31,7 @@ export class BscsController extends Controller {
      * Initiates the BCSC login by redirecting the user to the authorization URL.
      * @param redirectResponse - TSOA response object for redirection (307).
      * @param serverErrorResponse - TSOA response object for handling server errors (500).
+     * @param siteid - The site identifier passed as a query parameter.
      */
     @Get('/')
     public async initiateLogin(
@@ -40,12 +41,15 @@ export class BscsController extends Controller {
             500,
             { success: boolean; error: string }
         >,
+        @Query() siteid: string,
     ): Promise<void> {
         try {
             // Get the authorization URL with the 'bcsc' identity provider
             const authUrl = await getAuthorizationUrl({
                 identity_provider: 'bcsc',
+                siteId: siteid,
             });
+
             // Redirect with TsoaResponse
             redirectResponse(307, undefined, { Location: authUrl });
         } catch (err) {
@@ -62,32 +66,34 @@ export class BscsController extends Controller {
 
     /**
      * Handles the callback after the user authenticates with BCSC.
+     * @param typeORMErrorResponse - TSOA response for handling TypeORM errors (422).
      * @param successResponse - TSOA response object for successful user info retrieval (200).
-     * @param invalidTokenErrorResponse - TSOA response object for handling invalid token errors (400).
-     * @param serverErrorResponse - TSOA response object for handling server errors (500).
+     * @param badRequestErrorResponse - TSOA response for handling bad requests (400).
+     * @param serverErrorResponse - TSOA response for handling server errors (500).
+     * @param pidOwnerNotFoundResponse - TSOA response for handling cases when PID is not found or Owner not matched (204).
      * @param code - The authorization code returned by the BCSC login.
+     * @param state - The state parameter, parsed to find the site ID for the user.
      */
     @Get('/userinfo')
     public async handleCallback(
+        @Res() typeORMErrorResponse: TsoaResponse<422, GenericTypeORMErrorType>,
         @Res() successResponse: TsoaResponse<200, UserInfoResponse>,
-        @Res()
-        invalidTokenErrorResponse: TsoaResponse<
-            400,
-            { success: boolean; error: string }
-        >,
-        @Res()
-        serverErrorResponse: TsoaResponse<
-            500,
-            { success: boolean; error: string }
-        >,
+        @Res() badRequestErrorResponse: TsoaResponse<400, badRequestError>,
+        @Res() serverErrorResponse: TsoaResponse<500, serverErrorType>,
+        @Res() pidOwnerNotFoundResponse: TsoaResponse<204, pidNotFound>,
         @Query() code: string,
-        // @Query() siteID: string,
+        @Query() state: string,
     ): Promise<void> {
         try {
+            let matchingOwner: any;
+
             if (!code)
                 throw new Error('Authorization code is missing or invalid'); // Check for a valid authorization code
 
-            // Step 1: Exchange the authorization code for tokens
+            const parsedState = JSON.parse(decodeURIComponent(state));
+            console.log(parsedState);
+
+            // Step 1: Exchange the authorization code for a token
             const tokenResponse = await fetch(
                 `${process.env.CSS_DOMAIN_NAME_URL}/token`,
                 {
@@ -110,7 +116,7 @@ export class BscsController extends Controller {
             if (!tokenData.access_token)
                 throw new Error('Token exchange failed'); // Ensure the token exchange was successful
 
-            // Step 2: Use the access token to request user info
+            // Step 2: Use the access token to request the users info
             const userInfoResponse = await fetch(
                 `${process.env.CSS_DOMAIN_NAME_URL}/userinfo`,
                 {
@@ -121,36 +127,112 @@ export class BscsController extends Controller {
             );
 
             const userInfo = await userInfoResponse.json();
+            // BCSC has a unique id but it's in the form of <id>@<bcsc_client_id>
+            const bcscId = userInfo.sub.split('@')[0];
 
-            logger.info(`User Info: ${JSON.stringify(userInfo)}`); // Log the user information
+            // Call the geocode API to get the user's address details
+            let res: getAddressResults = { results: [] };
+            res = await new GeocodeAPICaller().getAddress(
+                `${userInfo.address.street_address} ${userInfo.address.locality} ${userInfo.address.region}`,
+            );
 
-            // Respond with success, including tokens and user info
-            return successResponse(200, {
-                success: true,
-                token: tokenData.access_token,
-                refreshToken: tokenData.refresh_token,
-                userInfo,
-            });
-        } catch (err) {
-            if (err instanceof Error) {
-                logger.error(err.message); // Log the error message
-                if (
-                    err.message.includes(
-                        'Authorization code is missing or invalid',
-                    )
-                ) {
-                    // Handle invalid token error
-                    invalidTokenErrorResponse(400, {
-                        success: false,
-                        error: err.message,
+            // Check if any address result matches the site ID in the parsed state
+            const matchedResult = res.results.find(
+                (p: any) => p.siteID === parsedState.siteId,
+            );
+
+            if (matchedResult) {
+                const pidsData: any = await getPIDs(parsedState.siteId);
+                const pids = pidStringSplitAndSort(pidsData.data.pids);
+
+                // Fetch property details emulating VIEW_PIN permission
+                let ownerResults = await findPropertyDetails(pids, [
+                    'VIEW_PIN',
+                ]);
+
+                // we can shortcut here if the user info bcsc id is already added to the database
+                matchingOwner = ownerResults.find(
+                    (f: any) => f.bcscId === bcscId,
+                );
+
+                if (!matchingOwner) {
+                    // Iterate through owner results to calculate name matching weights
+                    ownerResults = ownerResults.map((m: any) => {
+                        return {
+                            ...m,
+                            nameWeight: compareNames(
+                                `${m.givenName} ${m.lastName_1} ${m.lastName_2}`,
+                                `${userInfo.given_names} ${userInfo.family_name}`,
+                            ),
+                        };
+                    });
+
+                    // Find the highest name weight to identify the best match
+                    const highestMatchingOwner = ownerResults.find(
+                        (f: any) =>
+                            f.nameWeight ===
+                            Math.max(
+                                ...ownerResults.map((m: any) => m.nameWeight),
+                            ),
+                    );
+
+                    if (highestMatchingOwner?.nameWeight > 80) {
+                        matchingOwner = highestMatchingOwner;
+                    }
+                }
+
+                if (matchingOwner) {
+                    // Update the active pin record with the user's BCSC ID
+                    await updateActivePin(
+                        { livePinId: matchingOwner.livePinId },
+                        { bcscId: bcscId },
+                    );
+
+                    // Respond with success, including relevant user information
+                    return successResponse(200, {
+                        success: true,
+                        pids: matchingOwner.pids,
+                        livePinId: matchingOwner.livePinId,
                     });
                 } else {
-                    // Handle server error
-                    serverErrorResponse(500, {
-                        success: false,
-                        error: err.message,
-                    });
+                    const exception: pidNotFound = {
+                        message: `Unable to match owner name of property`,
+                        code: 204,
+                    };
+                    throw exception;
                 }
+            } else {
+                const exception: pidNotFound = {
+                    message: `BCSC User Address does not match SiteID Address`,
+                    code: 204,
+                };
+                throw exception;
+            }
+        } catch (err: any) {
+            if (err instanceof TypeORMError) {
+                logger.warn(
+                    `Encountered TypeORM Error updating user during BCSC login: ${err.message}`,
+                );
+                return typeORMErrorResponse(422, { message: err.message });
+            }
+            if (err.code === 400) {
+                logger.warn(
+                    `Encountered 400 bad request error in getPropertyDetails: ${err.message}`,
+                );
+                return badRequestErrorResponse(400, {
+                    message: err.message,
+                    code: err.code,
+                });
+            } else if (err.code === 204) {
+                return pidOwnerNotFoundResponse(204, {
+                    message: err.message,
+                    code: err.code,
+                });
+            } else {
+                logger.warn(
+                    `Encountered 500 unknown Internal Server Error in getPropertyDetails: ${err.message}`,
+                );
+                return serverErrorResponse(500, { message: err.message });
             }
         }
     }
